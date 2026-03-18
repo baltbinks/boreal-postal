0404f6e32a8ed4403e0f70d43c9b8d9397952248
Paul <paularnacfr@gmail.com>
2026-03-18 07:14:43 -0400

    Fix host authorization: set config.hosts = nil in application.rb

--- config/application.rb
+++ config/application.rb
@@ -48,3 +48,6 @@ module Postal
 
   end
 end
+
+    # Allow all hosts for API access
+    config.hosts = nil
--- config/initializers/host_authorization.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Allow all hosts for API access. Postal instances are managed
-# via API and accessed from various hostnames/IPs.
-Rails.application.config.hosts.clear
