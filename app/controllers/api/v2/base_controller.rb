e6bdca7c848267afd75d9a3dc4118e143cea02b1
Paul <paularnacfr@gmail.com>
2026-03-18 07:02:12 -0400

    Fix namespace: use API (acronym) instead of Api for Zeitwerk compatibility

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Api
+module API
   module V2
     class BaseController < ActionController::Base
       skip_before_action :verify_authenticity_token
--- app/controllers/api/v2/webhooks_controller.rb
+++ app/controllers/api/v2/webhooks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Api
+module API
   module V2
     class WebhooksController < BaseController
 
